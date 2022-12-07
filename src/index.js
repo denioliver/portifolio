a2f433d63abd571ee714743698c2ff98d3cbd63a
Denivan Oliveira <denioliver777@gmail.com>
2022-12-06 21:47:38 -0300

    build

--- src/index.js
+++ src/index.js
@@ -4,12 +4,11 @@ import './estilo.css'
 import ReactDOM from 'react-dom';
 import React from 'react';
 
-import Cabecalho from './componentes/Cabecalho';
-import Sobre_mim from './componentes/Sobre_mim';
+import Cabecalho from './componentes/Cabecalho'
+import Sobre_mim from './componentes/Sobre_mim'
 import Projetos from './componentes/Projetos'
-import Contatos from './componentes/Contatos';
-import Rodape from './componentes/Rodape';
-
+import Contatos from './componentes/Contatos'
+import Rodape from './componentes/Rodape'
 
 ReactDOM.render(
 
